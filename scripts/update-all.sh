e01e3adbd84aca1e8b5622a1d52e983b6837944d
Mikko Vestola <mikko.vestola@iki.fi>
2021-07-30 15:55:10 +0300

    Fix scripts and update garmin edge GPX

--- scripts/update-all.sh
+++ scripts/update-all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Updating all data from exported Google Maps KML..."
-./kml-to-osm.sh
+./kml-to-garmin-edge-800-gpx.sh
 ./kml-to-gpx.sh
+./kml-to-osm.sh
 ./kml-to-umap-compatible-kml.sh
 echo "All done"
